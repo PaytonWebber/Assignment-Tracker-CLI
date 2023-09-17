2683afb96ddf09440bbd9f12d58d35ada5c1dbe6
Payton Webber <paytonwebber@gmail.com>
2023-09-16 19:25:51 -0700

    Fixed bug with config parser

--- assignment_tracker/config.py
+++ assignment_tracker/config.py
@@ -13,10 +13,5 @@ except KeyError:
     print("Error: config.ini is missing required fields. Please check the README for more information.")
     exit(1)
 
-# Check if the config has classes and assignments
-if 'Classes' not in config or 'Assignments' not in config:
-    print("Error: config.ini is missing required fields. Please check the README for more information.")
-    exit(1)
-
 CLASSES = [class_name for class_name in config['Classes'].values()]
 ASSIGNMENTS = [(assignment_name, class_name) for assignment_name, class_name in zip(config['Assignments'].values(), config['Assignments'].keys())]
