87f30a3b19d137caa93349ff2ae04798b666fc1d
Payton Webber <paytonwebber@gmail.com>
2023-09-16 19:37:32 -0700

    Added functionality to the config parser.

--- assignment_tracker/config.py
+++ assignment_tracker/config.py
@@ -1,6 +1,7 @@
 # config.py
 
 import configparser
+from rich.prompt import Prompt
 
 # Load the configuration from config.ini
 config = configparser.ConfigParser()
@@ -8,10 +9,19 @@ config.read('config.ini')
 
 try:
     CSV_FILE = config['General']['CSV_FILE']
+except KeyError:
+    CSV_FILE = Prompt.ask('[bold blue]Please enter the path to your CSV file.[/bold blue]')
+    config['General']['CSV_FILE'] = CSV_FILE
+    with open('config.ini', 'w') as f:
+        config.write(f)
+
+try:
     DAYS_NOTICE = int(config['General']['DAYS_NOTICE'])
 except KeyError:
-    print("Error: config.ini is missing required fields. Please check the README for more information.")
-    exit(1)
-
+    DAYS_NOTICE = Prompt.ask('[bold blue]Please enter the number of days notice you would like to receive.[/bold blue]')
+    config['General']['DAYS_NOTICE'] = str(DAYS_NOTICE)
+    with open('config.ini', 'w') as f:
+        config.write(f)
+    
 CLASSES = [class_name for class_name in config['Classes'].values()]
 ASSIGNMENTS = [(assignment_name, class_name) for assignment_name, class_name in zip(config['Assignments'].values(), config['Assignments'].keys())]
